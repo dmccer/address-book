0411d0cf41115d69a9ce6c3d685c64e6b47e1eac
dmccer <dmccer@gmail.com>
2016-01-06 17:40:36 +0800

    我的页面

--- src/asset/js/my/index.js
+++ src/asset/js/my/index.js
@@ -1,5 +1,6 @@
 import '../../less/global/global.less';
 import '../../less/component/layout.less';
+import '../../less/component/form.less';
 import './index.less';
 
 import React from 'react';
@@ -15,8 +16,27 @@ export default class MyPage extends React.Component {
 
   render() {
     return (
-      <section className="my">
+      <section className="my-page">
         <Header title="我的" />
+        <section className="my">
+          <div className="avatar">
+            <a href="#"></a>
+          </div>
+          <ul className="vip-score grid">
+            <li className="vip">
+              <span>当前等级:</span>
+              <b>VIP 1</b>
+            </li>
+            <li className="score">
+              <span>您的积分:</span>
+              <b><a href="#">357</a></b>
+            </li>
+          </ul>
+          <div className="btns">
+            <button className="btn block red" type="button">签到</button>
+            <button className="btn block line" type="button">退出账号</button>
+          </div>
+        </section>
         <Nav />
       </section>
     );
