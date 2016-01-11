9983134fb064b6d2c73db0e8d211751b66c40d16
dmccer <dmccer@gmail.com>
2016-01-11 13:51:14 +0800

    新建按钮

--- src/asset/js/header/index.js
+++ src/asset/js/header/index.js
@@ -6,6 +6,25 @@ import ReactDOM from 'react-dom';
 export default class Header extends React.Component {
   constructor() {
     super();
+
+    this.state = {};
+  }
+
+  handleAdd() {
+    this.setState({
+      adding: !this.state.adding
+    });
+  }
+
+  renderAddActions() {
+    if (this.state.adding) {
+      return (
+        <ul className="actions">
+          <li><a href="#">新建通讯录</a></li>
+          <li><a href="#">新建名片</a></li>
+        </ul>
+      );
+    }
   }
 
   render() {
@@ -13,7 +32,10 @@ export default class Header extends React.Component {
       <header className="header row">
         <section className="left"><i className="icon icon-mail"></i></section>
         <section className="center">{this.props.title}</section>
-        <section className="right"><i className="icon s20 icon-plus"></i></section>
+        <section className="right" onClick={this.handleAdd.bind(this)}>
+          <i className="icon s20 icon-plus"></i>
+          {this.renderAddActions()}
+        </section>
       </header>
     );
   }
