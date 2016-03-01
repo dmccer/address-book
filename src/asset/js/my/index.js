77e935ba7c0ab772e61ed7266777d52860d6a4d9
dmccer <dmccer@gmail.com>
2016-03-01 14:48:38 +0800

    我的页面逻辑

--- src/asset/js/my/index.js
+++ src/asset/js/my/index.js
@@ -1,3 +1,8 @@
+/**
+ * MyPage 我的页面
+ *
+ * @author Kane xiaoyunhua@ttyhuo.cn
+ */
 import '../../less/global/global.less';
 import '../../less/component/layout.less';
 import '../../less/component/form.less';
@@ -12,14 +17,21 @@ import Header from '../header/';
 import Nav from '../nav/';
 import Loading from '../loading/';
 import Popover from '../popover/';
+import Toast from '../toast/';
+import Log from '../log/';
 
 export default class MyPage extends React.Component {
+  state = {
+    signinable: false,
+    account: {}
+  };
+
   constructor() {
     super();
+  }
 
-    this.state = {
-      signinable: true
-    }
+  componentDidMount() {
+    this.getUser();
   }
 
   /**
@@ -35,53 +47,98 @@ export default class MyPage extends React.Component {
 
     new Promise((resolve, reject) => {
       $.ajax({
-        url: '/api/signin',
+        url: '/mvc/pim/book_in',
         type: 'POST',
         success: resolve.bind(this),
         error: reject.bind(this)
       });
     }).then((res) => {
-      this.setState({
-        signinable: false
-      });
+      if (res.retcode === 0) {
+        this.setState({
+          signinable: false
+        });
 
-      this.refs.popover.success('签到成功');
-    }).catch((xhr) => {
-      console.log(xhr)
+        this.refs.popover.success('签到成功');
+        return;
+      }
+
+      this.refs.toast.error(res.msg);
+    }).catch((err) => {
+      Log.error(err);
+
+      this.refs.toast.error('签到失败');
     }).done(() => {
       this.refs.loading.close();
-    })
+    });
+  }
+
+  getUser() {
+    this.refs.loading.show('加载中...');
+
+    new Promise((resolve, reject) => {
+      $.ajax({
+        url: '/mvc/pim/fetch_uinfo',
+        type: 'POST',
+        success: resolve.bind(this),
+        error: reject.bind(this)
+      });
+    }).then((res) => {
+      if (res.retcode === 0) {
+        this.setState({
+          signinable: !res.book_in,
+          account: res.userInfo
+        });
+
+        return;
+      }
+
+      this.refs.toast.warn(res.msg);
+    }).catch((err) => {
+      Log.error(err);
+
+      this.refs.toast.error(`加载用户信息出错, ${err.message}`);
+    }).done(() => {
+      this.refs.loading.close();
+    });
   }
 
   render() {
+    let account = this.state.account;
     return (
       <section className="my-page">
         <Header title="我的" />
         <section className="my">
           <div className="avatar">
             <a href="#" style={{
-              backgroundImage: 'url("http://imgsize.ph.126.net/?imgurl=http://img0.ph.126.net/DumDJ-ZwuOleqNA40O14ew==/6631345942143875871.jpg_230x230x1x95.jpg")',
+              backgroundImage: `url(${account.photo || 'http://imgsize.ph.126.net/?imgurl=http://img1.ph.126.net/3rXw868LDcauo0vm_tcVtQ==/6598101108623504221.jpg_188x188x1.jpg'})`,
               backgroundSize: 'contain'
             }}></a>
           </div>
           <ul className="vip-score grid">
             <li className="vip">
               <span>当前等级:</span>
-              <b>VIP 1</b>
+              <b>VIP {account.level}</b>
             </li>
             <li className="score">
               <span>您的积分:</span>
-              <b><a href="./score-rule.html">357</a></b>
+              <b><a href="./score-rule.html">{account.score}</a></b>
             </li>
           </ul>
           <div className="btns">
-            <button className="btn block red" type="button" disabled={!this.state.signinable} onClick={this.handleSignin.bind(this)}>签到</button>
-            <button className="btn line blue" type="button">退出账号</button>
+            <button
+              className="btn block red sigin-btn"
+              type="button"
+              disabled={!this.state.signinable}
+              onClick={this.handleSignin.bind(this)}>{this.state.signinable ? '签到' : '已签到'}</button>
+            {
+              // <button className="btn line blue" type="button">退出账号</button>
+            }
           </div>
           <Popover ref="popover" />
         </section>
         <Nav on="account" />
         <Loading ref="loading" />
+        <Toast ref="toast" />
       </section>
     );
   }
