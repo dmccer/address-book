a524fec9316c98199f1653ec0acfc1d07927185a
dmccer <dmccer@gmail.com>
2016-03-03 18:39:42 +0800

    交换名片主名片展示

--- src/asset/js/biz-card/swap/index.js
+++ src/asset/js/biz-card/swap/index.js
@@ -9,14 +9,65 @@ import './index.less';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Promise from 'promise';
 
+import AjaxError from '../../ajax-err/';
 import Header from '../../header/';
 import Nav from '../../nav/';
 import MiniCard from '../mini-card/';
+import Loading from '../../loading/';
+import Toast from '../../toast/';
+import Log from '../../log/';
 
 export default class BizCardSwapPage extends React.Component {
+  state = {
+    bizCard: {}
+  };
+
   constructor() {
     super();
+  }
+
+  componentDidMount() {
+    AjaxError.init(this.refs.toast);
+
+    this.getMyBizCard();
+  }
+
+  /**
+   * getMyBizCard 获取我的主名片信息
+   * @return {Promise}
+   */
+  getMyBizCard() {
+    this.refs.loading.show('加载中...');
+
+    new Promise((resolve, reject) => {
+      $.ajax({
+        url: '/mvc/pim/query_user_card_desc',
+        type: 'GET',
+        cache: false,
+        success: resolve,
+        error: reject
+      });
+    }).then((res) => {
+      if (res.retcode === 0) {
+        this.setState({
+          bizCard: res.card
+        });
+        
+        return;
+      }
+
+      this.refs.toast.warn(res.msg);
+    }).catch((err) => {
+      if (err && err instanceof Error) {
+        Log.error(err);
+
+        this.refs.toast.warn(`加载出错,${err.message}`);
+      }
+    }).done(() => {
+      this.refs.loading.close();
+    });
   }
 
   render() {
@@ -24,7 +75,7 @@ export default class BizCardSwapPage extends React.Component {
       <section className="biz-card-swap-page">
         <Header title="我的名片" />
         <div className="biz-card-swap">
-          <MiniCard />
+          <MiniCard {...this.state.bizCard} />
           <ul className="menu grid">
             <li><a href="./my-biz-card.html">名片好友</a></li>
             <li className="on">名片交换</li>
@@ -42,6 +93,8 @@ export default class BizCardSwapPage extends React.Component {
           <div className="fixed-holder"></div>
         </div>
         <Nav on="biz-card" />
+        <Loading ref="loading" />
+        <Toast ref="toast" />
       </section>
     );
   }
