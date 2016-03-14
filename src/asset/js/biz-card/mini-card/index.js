c3def7525f58d5837a139bd7ca4f592bf1b94872
dmccer <dmccer@gmail.com>
2016-03-14 17:10:11 +0800

    名片项

--- src/asset/js/biz-card/mini-card/index.js
+++ src/asset/js/biz-card/mini-card/index.js
@@ -14,15 +14,16 @@ export default class MiniCard extends React.Component {
 	render() {
 		let props = this.props;
 		let certified = props.verifyflag === 2 ? <i className="icon s14 icon-certificate"></i> : null;
+		let holder = props.holder ? '发起人' : '';
 
 		return (
-			<a className="my-profile" href={`./biz-card-detail.html?cid=${props.cid}&uid=${props.uid}`}>
+			<a className="cell my-profile" onClick={props.onView} href={`./biz-card-detail.html?cid=${props.cid}&uid=${props.uid}`}>
 		    <div className="avatar" style={{
 		      backgroundImage: `url(${props.photo})`
 		    }}></div>
 		    <div className="profile">
 		      <p className="my">
-		        <span>{props.nikename}</span>
+		        <span>{props.nikename} {holder}</span>
 		        <span className="vip-level">VIP {props.level}</span>
 		      </p>
 		      <p className="intro">{props.desc}</p>
