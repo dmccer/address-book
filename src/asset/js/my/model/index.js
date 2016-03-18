23edab92085851aee2f02e81b3095118b8cf5aea
dmccer <dmccer@gmail.com>
2016-03-18 09:18:16 +0800

    用户model

--- src/asset/js/my/model/index.js
+++ src/asset/js/my/model/index.js
@@ -1,5 +1,9 @@
 import Promise from 'promise';
 
+/**
+ * 查询最近积分记录
+ * @return {Promise}
+ */
 export var MyRecentScoreActionList = () => {
   return new Promise((resolve, reject) => {
     $.ajax({
@@ -12,12 +16,42 @@ export var MyRecentScoreActionList = () => {
   });
 }
 
+/**
+ * 查询我的实名认证信息
+ * @return {Promise}
+ */
 export var MyVerifyInfo = () => {
   return new Promise((resolve, reject) => {
     $.ajax({
-      url: '/pim/query_my_card_verify',
+      url: '/pim/query_user_verify',
       type: 'GET',
       cache: false,
+      success: resolve,
+      error: reject
+    });
+  });
+}
+
+export var RevokeMyVerify = () => {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: '/pim/revoke_user_verify',
+      type: 'POST',
+      success: resolve,
+      error: reject
+    });
+  });
+}
+
+export var UploadMyVerify = (bizCard, IDCard) => {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: '/pim/upload_user_verify',
+      type: 'POST',
+      data: {
+        namecard_mid: bizCard,
+        idcard_mid: IDCard
+      },
       success: resolve,
       error: reject
     });
